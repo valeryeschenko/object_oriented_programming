34afaaee3cc725c40a1f21001aa64762147c0ed3
Valery Eschenko <79979066+valeryeschenko@users.noreply.github.com>
2022-10-23 19:01:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,20 +8,13 @@ class Student:
         self.grades = {}
 
     def rate_lecturer(self, lecturer, course, grade):
-        if isinstance(lecturer,
-                      Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
-
-    def __str__(self):
-        return f'Имя: {self.name}\nSurname: {self.surname}\n' \
-               f'Средняя оценка за домашние задания: {self.gpa_student}\n' \
-               f'Курсы в процессе изучения: {self.courses_in_progress}\n' \
-               f'Завершенные курсы: {self.finished_courses}'
 
     @property
     def gpa_student(self):
@@ -34,9 +27,29 @@ class Student:
             return 0
         return sum_num / count
 
+    def __str__(self):
+        return f'Имя: {self.name}\n'\
+               f'Surname: {self.surname}\n'\
+               f'Средняя оценка за домашние задания: {self.gpa_student}\n' \
+               f'Курсы в процессе изучения: {",".join(self.courses_in_progress)}\n' \
+               f'Завершенные курсы: {",".join(self.finished_courses)}'
+
+    def __lt__(self, other):
+        std_1 = self.gpa_student
+        std_2 = other.gpa_student
+        return std_1 < std_2
+
+    def __le__(self, other):
+        std_1 = self.gpa_student
+        std_2 = other.gpa_student
+        return std_1 <= std_2
+
+    def __eq__(self, other):
+        std_1 = self.gpa_student
+        std_2 = other.gpa_student
+        return std_1 == std_2
 
 class Mentor:
-
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
@@ -44,11 +57,12 @@ class Mentor:
 
 
 class Lecturer(Mentor):
-    def __init__(self):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
         self.grades = {}
 
     def __str__(self):
-        return f"Имя: {self.name}\nSurname: {self.surname}\nСредняя оценка за лекции: {self.gpa_lecturer}"
+        return f'Имя: {self.name}\nSurname: {self.surname}\nСредняя оценка за лекции: {self.gpa_lecturer}'
 
     @property
     def gpa_lecturer(self):
@@ -61,8 +75,23 @@ class Lecturer(Mentor):
             return 0
         return sum_num / count
 
+    def __lt__(self, other):
+        ltr_1 = self.gpa_lecturer
+        ltr_2 = other.gpa_lecturer
+        return ltr_1 < ltr_2
+
+    def __le__(self, other):
+        ltr_1 = self.gpa_lecturer
+        ltr_2 = other.gpa_lecturer
+        return ltr_1 <= ltr_2
+
+    def __eq__(self, other):
+        ltr_1 = self.gpa_lecturer
+        ltr_2 = other.gpa_lecturer
+        return ltr_1 == ltr_2
 
 class Reviewer(Mentor):
+    pass
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -76,3 +105,32 @@ class Reviewer(Mentor):
     def __str__(self):
         return f"Имя: {self.name}\nSurname: {self.surname}\n"
 
+# Студенты
+best_student_roy = Student('Roy', 'Eman', 'man')
+best_student_roy.courses_in_progress += ['Python', 'Sql', 'HTML']
+
+best_student_nicol = Student('Nicol', 'Jonson', 'woman')
+best_student_nicol.courses_in_progress += ['Pyton', 'Git']
+
+# Лекторы
+lecturer_mentor_sam = Lecturer('Sam', 'Buddy')
+lecturer_mentor_sam.courses_attached += ['Python', 'Git']
+
+lecturer_mentor_nik = Lecturer('Nik', 'Popins')
+lecturer_mentor_nik.courses_attached += ['HTML', 'Sql']
+
+# Проверяющие
+reviewer_mentor_jack = Reviewer('Jack', 'Gibbs')
+reviewer_mentor_jack.courses_attached += ['Python', 'Sql']
+
+reviewer_mentor_bob = Reviewer('Bob', 'Simons')
+reviewer_mentor_bob.courses_attached += ['HTML', 'Git']
+
+# Оценки
+best_student_roy.rate_lecturer(lecturer_mentor_sam, 'Python', 10)
+best_student_nicol.rate_lecturer(lecturer_mentor_sam, 'Git', 10)
+
+reviewer_mentor_jack.rate_hw(best_student_roy, 'Sql', 10)
+reviewer_mentor_bob.rate_hw(best_student_nicol, 'HTML', 10)
+
+print(best_student_nicol)
